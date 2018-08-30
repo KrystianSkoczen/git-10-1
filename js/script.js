be0dc6a22166a67bd006b7d8050ea1975ba544eb
skoczenkrystian <skoczenkrystian@gmail.com>
2018-08-30 19:51:51 +0200

    add progress bar

--- js/script.js
+++ js/script.js
@@ -6,6 +6,13 @@ var flkty = new Flickity( '.main-carousel', {
     hash: true
 });
 
+var progressBar = document.querySelector('.progress-bar')
+
+flkty.on( 'scroll', function( progress ) {
+ 	progress = Math.max( 0, Math.min( 1, progress ) );
+	progressBar.style.width = progress * 100 + '%';
+});
+
 $('.reset').on( 'click', function() {
     flkty.select(0);
 });
